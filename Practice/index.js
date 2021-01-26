fb4c1d7af41fea4bf696e1b85c6d053fe3f282f8
leejunseon <ljs921026@gmail.com>
2021-01-26 21:59:45 +0900

    2021-01-26

--- Practice/index.js
+++ Practice/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+//const title = document.getElementById("title");
+const title=document.querySelector("#title");
+title.innerHTML="Hi! From JS";
+title.style.color="red";
+document.title="title";
+console.dir(document);
+
+/*
+function handleResize(event){
+    //console.log("I have been resized");
+    console.log(event);
+}
+
+window.addEventListener("resize",handleResize);
+*/
+
+function handleClick(){
+    title.style.color="blue";
+}
+
+title.addEventListener("click",handleClick);
