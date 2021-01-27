10820fcf5fca0501a1da40b1a14ccc54f80d27fc
leejunseon <ljs921026@gmail.com>
2021-01-27 23:15:30 +0900

    2021-01-27

--- Practice/index.js
+++ Practice/index.js
@@ -1,9 +1,11 @@
 //const title = document.getElementById("title");
+/*
 const title=document.querySelector("#title");
 title.innerHTML="Hi! From JS";
 title.style.color="red";
 document.title="title";
 console.dir(document);
+*/
 
 /*
 function handleResize(event){
@@ -14,8 +16,41 @@ function handleResize(event){
 window.addEventListener("resize",handleResize);
 */
 
+const title=document.querySelector("#title");
+
+/*
+const BASE_COLOR="rgb(52, 73, 94)";
+const OTHER_COLOR="#7f8c8d";
+
 function handleClick(){
-    title.style.color="blue";
+    const currentColor=title.style.color;
+    if(currentColor===BASE_COLOR){
+        title.style.color=OTHER_COLOR;
+    }else{
+        title.style.color=BASE_COLOR;
+    }
+}
+*/ 
+
+const CLICKED_CLASS="clicked";
+
+function handleClick(){
+    //const currentClassname=title.className;
+    const hasClass=title.classList.contains(CLICKED_CLASS);
+    if(/*currentClassname!==CLICKED_CLASS*/ !hasClass){
+        //title.className=CLICKED_CLASS;
+        title.classList.add(CLICKED_CLASS);
+    }else{
+        //title.className="";
+        title.classList.remove(CLICKED_CLASS);
+    }
 }
 
-title.addEventListener("click",handleClick);
+function handleClickToggle(){
+    title.classList.toggle(CLICKED_CLASS);
+}
+
+function init(){
+    title.addEventListener("click",/*handleClick*/ handleClickToggle);
+}
+init();
